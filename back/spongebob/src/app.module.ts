bd90a2615bf9347ba4cb7c6d22f399dcd136f614
nsscat <nsscat@naver.com>
2022-03-10 10:51:09 +0900

    back초기 세팅

--- back/spongebob/src/app.module.ts
+++ back/spongebob/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    //   charset: process.env.DB_CHARSET,
+    //   timezone: process.env.DB_TIMEZONE,
+    //   synchronize: false,
+    //   logging: true,
+    //   entities: [],
+    //   keepConnectionAlive: true,
+    //   autoLoadEntities: true,
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- back/spongebob/src/main.ts
+++ back/spongebob/src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    allowedHeaders: 'Content-Type',
+    methods: 'POST,GET,PUT,PATCH,DELETE,OPTIONS',
+    credentials: true,
+    origin: true,
+  });
+
+  app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle('SponGeBoB API')
+    .setDescription('SponGeBoB API Document')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'SponGeBoB API',
+  });
+
+  const port = process.env.NODE_ENV === 'prod' ? 80 : 8000;
+
+  await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  console.log(`listening on port ${port}`);
 }
 bootstrap();
